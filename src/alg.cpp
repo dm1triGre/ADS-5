077c6bb4688450dd415be1265120f8be536eb298
dm1triGre <dmitrijgrechin@gmail.com>
2025-06-07 18:05:10 +0300

    b87

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,89 +4,75 @@
 #include "tstack.h"
 
 std::string infx2pstfx(const std::string& inf) {
-    TStack<char, 100> stack;
     std::string postfix;
-    std::string number;
-
+    TStack<char, 100> stack;
+    std::map<char, int> priority = {
+        {'+', 1}, {'-', 1},
+        {'*', 2}, {'/', 2}
+    };
     for (size_t i = 0; i < inf.size(); ++i) {
-        char ch = inf[i];
-        if (isdigit(ch)) {
-            number += ch;
+        char c = inf[i];
+        if (c == ' ') continue;
+        if (isdigit(c)) {
+            while (i < inf.size() && isdigit(inf[i])) {
+                postfix += inf[i++];
+            }
+            postfix += ' ';
+            i--;
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (c == ')') {
+            while (!stack.isEmpty() && stack.peek() != '(') {
+                postfix += stack.pop();
+                postfix += ' ';
+            }
+            stack.pop();
         } else {
-            if (!number.empty()) {
-                postfix += number + " ";
-                number.clear();
+            while (!stack.isEmpty() && stack.peek() != '(' &&
+                priority[stack.peek()] >= priority[c]) {
+                postfix += stack.pop();
+                postfix += ' ';
             }
-
-            if (ch == '(') {
-                stack.push(ch);
-            } else if (ch == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    postfix += std::string(1, stack.pop()) + " ";
-                }
-                stack.pop();
-            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    char top = stack.peek();
-                    bool shouldPop = false;
-                    if (ch == '+' || ch == '-') {
-                        shouldPop = true;
-                    } else if (top == '*' || top == '/') {
-                        shouldPop = true;
-                    }
-                    if (!shouldPop) break;
-                    postfix += std::string(1, stack.pop()) + " ";
-                }
-                stack.push(ch);
-            }
+            stack.push(c);
         }
     }
-
-    if (!number.empty()) {
-        postfix += number + " ";
-    }
-
     while (!stack.isEmpty()) {
-        postfix += std::string(1, stack.pop()) + " ";
+        postfix += stack.pop();
+        postfix += ' ';
     }
-
     if (!postfix.empty() && postfix.back() == ' ') {
         postfix.pop_back();
     }
-
     return postfix;
 }
 
 int eval(const std::string& post) {
     TStack<int, 100> stack;
-    std::string token;
-
-    for (size_t i = 0; i < post.size(); ++i) {
-        char ch = post[i];
-        if (isdigit(ch)) {
-            token.clear();
-            while (i < post.size() && isdigit(post[i])) {
-                token += post[i++];
+    std::string numStr;
+    for (char c : post) {
+        if (c == ' ') {
+            if (!numStr.empty()) {
+                stack.push(std::stoi(numStr));
+                numStr.clear();
             }
-            if (!token.empty()) {
-                stack.push(std::stoi(token));
+            continue;
+        }
+        if (isdigit(c)) {
+            numStr += c;
+        } else {
+            if (!numStr.empty()) {
+                stack.push(std::stoi(numStr));
+                numStr.clear();
             }
-            i--;
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            if (stack.isEmpty()) throw "Not enough operands!";
-            int b = stack.pop();
-            if (stack.isEmpty()) throw "Not enough operands!";
-            int a = stack.pop();
-
-            switch (ch) {
-                case '+': stack.push(a + b); break;
-                case '-': stack.push(a - b); break;
-                case '*': stack.push(a * b); break;
-                case '/': stack.push(a / b); break;
+            int right = stack.pop();
+            int left = stack.pop();
+            switch (c) {
+                case '+': stack.push(left + right); break;
+                case '-': stack.push(left - right); break;
+                case '*': stack.push(left * right); break;
+                case '/': stack.push(left / right); break;
             }
         }
     }
-
-    if (stack.isEmpty()) throw "No result!";
     return stack.pop();
 }
